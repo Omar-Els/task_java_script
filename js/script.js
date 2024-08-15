03dc71402387ad1399782effb120c6a6f91b718d
omar <oe21741@gmail.com>
2024-08-15 17:18:42 +0300

    creating a task_js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var container_fluid = document.querySelector(".container-fluid")
+
+var all_light_dark = document.querySelectorAll(".input-group")
+var light = document.querySelector(".btn-light")
+var dark = document.querySelector(".btn-dark")
+
+var allproducts = document.querySelectorAll(".list li")
+
+var div1 = document.querySelector("#div1")
+var div2 = document.querySelector(".div2")
+var btn1 = document.querySelector("#btnprice")
+
+var totalprice = 0
+
+all_light_dark.forEach(function (light_dark) {
+    dark.onclick = function (){
+        container_fluid.style.color="white";
+        container_fluid.style.background="black";
+    }
+    light.onclick = function (){
+        container_fluid.style.color="black";
+        container_fluid.style.background="white";
+    }
+});
+
+allproducts.forEach(function (item){
+    item.onclick = function (){
+        totalprice += +(item.getAttribute("price"))
+        div1.innerHTML += item.textContent + ' - '
+
+        if (div1.innerHTML != ""){
+            btn1.style.display="block";
+        }
+    }
+})
+
+btn1.onclick = function (){
+    div2.innerHTML = "total price: "+totalprice;
+}

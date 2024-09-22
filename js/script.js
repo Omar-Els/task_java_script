be7c79f3916064b7678963fc3693f3e47a8c86b4
Amr maher <amrr.maherr24@gmail.com>
2024-09-22 16:15:43 +0300

    Add body variable and transition effect for body on dark mode toggle

--- js/script.js
+++ js/script.js
@@ -11,15 +11,17 @@ var div2 = document.querySelector(".div2")
 var btn1 = document.querySelector("#btnprice")
 
 var totalprice = 0
-
+let body = document.querySelector('body')
 all_light_dark.forEach(function (light_dark) {
     dark.onclick = function (){
         container_fluid.style.color="white";
         container_fluid.style.background="black";
+        body.style.transition = '0.05s'
     }
     light.onclick = function (){
         container_fluid.style.color="black";
         container_fluid.style.background="white";
+        body.style.transition = '0.05s'
     }
 });
 
